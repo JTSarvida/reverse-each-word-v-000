4011cb71370af483a0221c363cd1f37925a76e40
Joshua Tan <jtsarvida@gmail.com>
2018-05-04 14:27:25 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,13 @@
 def reverse_each_word(sentence)
+<<<<<<< HEAD
   wow = sentence.split.collect do |words|
     words.reverse
   end
   wow.join(" ")
+=======
+  sentence.collect do |words|
+    words.reverse.join(" ")
+    
+  end
+>>>>>>> 3be41625646927548a18696182cd2adc6037c87f
 end
